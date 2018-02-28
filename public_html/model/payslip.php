85c704860eb497cfea5d4f1280714bf4e8bc67e3
andrea <eandram@hotmail.it>
2018-02-28 18:46:24 +0000

    icon menu and button

--- public_html/model/payslip.php
+++ public_html/model/payslip.php
@@ -6,6 +6,21 @@ if (!isset($_SESSION['admin']) || !isset($_SESSION['uName'])) {
     header( 'refresh:4;url=index.php' );
 }else{
     $variables=include_once __DIR__ . '/../templates/arrays/payslip.php';
+    $employee=array();
+    $query ="SELECT E.id_employee,concat_ws(' ',E.name,E.surname  ) as employee
+FROM employees E
+JOIN users U 
+WHERE U.users_id=E.user_id AND U.adminaccess='0'";
+    if ($result=$mysqli->query($query)){
+        while ($row=$result->fetch_assoc()){
+                $employee[]=$row;
+        }
+        $result->free();
+        $variables['employee']=$employee;
+
+    }else{
+        die($mysqli->error);
+    }
 
     echo $twig->render($template->getTemplate(),$variables);
 }
