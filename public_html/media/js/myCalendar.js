8db12740228701a8600708c4e86d53a36c0eff57
amuso01 <eandram@hotmail.it>
2018-04-16 10:42:37 +0100

    sql dab creation

--- public_html/media/js/myCalendar.js
+++ public_html/media/js/myCalendar.js
@@ -2,8 +2,24 @@ $(function() {
 
   $('#calendar').fullCalendar({
     firstDay: 1,
-    events:'jsonService/holidayApi.php'
+    events:'jsonService/holidayApi.php',
+    eventClick: function(event) {
+      var decision = confirm("Do you really want to do that?");
+      if (decision) {
+        console.log(event.id);
+        // $.ajax({
+        //   type: "POST",
+        //   url: "delete_event.php",
+        //   data: "&id=" + event.id,
+        //   success: function(json) {
+        //     $('#calendar').fullCalendar('removeEvents', event.id);
+        //     alert("Updated Successfully");}
+        // });
+
+      }
+    }
   })
+
 
 
 });
