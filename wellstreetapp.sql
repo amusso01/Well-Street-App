52b4346d1d4305f7f36069f83cf10d5edb4bbbf1
andrea <eandram@hotmail.it>
2018-04-20 18:35:17 +0100

    sql file fix

--- wellstreetapp.sql
+++ wellstreetapp.sql
@@ -1,33 +1,29 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 20, 2018 at 05:24 PM
--- Server version: 5.7.14
--- PHP Version: 7.0.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: wellstreetapp
+-- ------------------------------------------------------
+-- Server version	5.7.14
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `wellstreetapp`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `employees`
 --
 
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employees` (
-  `id_employee` int(11) NOT NULL,
+  `id_employee` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `surname` varchar(45) NOT NULL,
   `gender` enum('m','f') NOT NULL,
@@ -42,215 +38,187 @@ CREATE TABLE `employees` (
   `department` enum('boh','foh') NOT NULL,
   `position` varchar(45) DEFAULT NULL,
   `holiday_allowance` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id_employee`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`users_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`id_employee`, `name`, `surname`, `gender`, `email`, `starting_date`, `nin`, `date_of_birthday`, `post_code`, `address`, `phone_number`, `payrate`, `department`, `position`, `holiday_allowance`, `user_id`) VALUES
-(1, 'andrea', 'musso', 'm', 'eandram@hotmail.it', '2018-01-01', 'sn040497b', '1983-02-02', 'n154ax', NULL, '07478821689', 8.5, 'boh', NULL, 23, 1),
-(73, 'andrea', 'musso', 'm', 'eandram@hotmail.it', '2018-04-20', 'sn040497b', '1983-07-08', 'se59qw', 'flat 25, mirlees court, 50 coldharbour lane, , , london,', '07478821689', 12.5, 'foh', 'chef', 16, 88);
-
--- --------------------------------------------------------
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'andrea','musso','m','eandram@hotmail.it','2018-01-01','sn040497b','1983-02-02','n154ax',NULL,'07478821689',8.5,'boh',NULL,23,1),(73,'andrea','musso','m','eandram@hotmail.it','2018-04-20','sn040497b','1983-07-08','se59qw','flat 25, mirlees court, 50 coldharbour lane, , , london,','07478821689',12.5,'foh','chef',16,88);
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `file_uploaded`
 --
 
+DROP TABLE IF EXISTS `file_uploaded`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `file_uploaded` (
-  `id_info` int(11) NOT NULL,
+  `id_info` int(11) NOT NULL AUTO_INCREMENT,
   `file_name` varchar(45) NOT NULL,
-  `file_info` varchar(155) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `file_info` varchar(155) DEFAULT NULL,
+  PRIMARY KEY (`id_info`)
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `file_uploaded`
+--
+
+LOCK TABLES `file_uploaded` WRITE;
+/*!40000 ALTER TABLE `file_uploaded` DISABLE KEYS */;
+/*!40000 ALTER TABLE `file_uploaded` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `holiday_request`
 --
 
+DROP TABLE IF EXISTS `holiday_request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `holiday_request` (
-  `holiday_id` int(11) NOT NULL,
+  `holiday_id` int(11) NOT NULL AUTO_INCREMENT,
   `holiday_start` date NOT NULL,
   `holiday_end` date NOT NULL,
   `employee_id` int(11) DEFAULT NULL,
-  `holiday_approved` enum('Y','N') NOT NULL DEFAULT 'N'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `holiday_approved` enum('Y','N') NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`holiday_id`),
+  KEY `employee_id_idx` (`employee_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=139 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `holiday_request`
+--
+
+LOCK TABLES `holiday_request` WRITE;
+/*!40000 ALTER TABLE `holiday_request` DISABLE KEYS */;
+/*!40000 ALTER TABLE `holiday_request` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `payslip`
 --
 
+DROP TABLE IF EXISTS `payslip`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `payslip` (
-  `payslip_id` int(11) NOT NULL,
+  `payslip_id` int(11) NOT NULL AUTO_INCREMENT,
   `file_name` varchar(45) NOT NULL,
   `month` tinyint(12) NOT NULL,
   `employee_id` int(11) NOT NULL,
-  `year` year(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `year` year(4) NOT NULL,
+  PRIMARY KEY (`payslip_id`),
+  KEY `employee_id_idx` (`employee_id`),
+  CONSTRAINT `employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id_employee`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `payslip`
+--
+
+LOCK TABLES `payslip` WRITE;
+/*!40000 ALTER TABLE `payslip` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payslip` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `schedule_rota`
 --
 
+DROP TABLE IF EXISTS `schedule_rota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `schedule_rota` (
   `shift_id` int(11) NOT NULL,
   `employee_id` int(11) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  KEY `shift_id_idx` (`shift_id`),
+  KEY `employee_id_idx` (`employee_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `schedule_rota`
+--
+
+LOCK TABLES `schedule_rota` WRITE;
+/*!40000 ALTER TABLE `schedule_rota` DISABLE KEYS */;
+/*!40000 ALTER TABLE `schedule_rota` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `shift`
 --
 
+DROP TABLE IF EXISTS `shift`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `shift` (
-  `id_shift` int(11) NOT NULL,
+  `id_shift` int(11) NOT NULL AUTO_INCREMENT,
   `start_time` time DEFAULT NULL,
   `finish_time` time DEFAULT NULL,
   `shift_length` int(11) DEFAULT NULL,
-  `is_off` tinyint(4) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `is_off` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_shift`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `shift`
 --
 
-INSERT INTO `shift` (`id_shift`, `start_time`, `finish_time`, `shift_length`, `is_off`) VALUES
-(1, '07:00:00', '15:00:00', 500, 0),
-(2, '09:00:00', '17:00:00', 500, 0),
-(3, '07:00:00', '14:00:00', 420, 0),
-(4, '11:00:00', '17:00:00', 360, 0),
-(5, NULL, NULL, NULL, 1);
-
--- --------------------------------------------------------
+LOCK TABLES `shift` WRITE;
+/*!40000 ALTER TABLE `shift` DISABLE KEYS */;
+INSERT INTO `shift` VALUES (1,'07:00:00','15:00:00',500,0),(2,'09:00:00','17:00:00',500,0),(3,'07:00:00','14:00:00',420,0),(4,'11:00:00','17:00:00',360,0),(5,NULL,NULL,NULL,1);
+/*!40000 ALTER TABLE `shift` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `users_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `password` varchar(150) NOT NULL,
-  `adminaccess` enum('0','1') NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `adminaccess` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`users_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=89 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`users_id`, `username`, `password`, `adminaccess`) VALUES
-(1, 'admin1', '$2y$10$Gs6MZ/eR0StXWyEJa8ZA9.9IyKhGI3Z6UxRLSFInw670wRJciHs0W', '1'),
-(88, 'users1', '$2y$10$pITJguXXrqZHYRNf1w69jumaUPIwHH8.R/ZliHIH4/EumfVDMazk.', '0');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin1','$2y$10$Gs6MZ/eR0StXWyEJa8ZA9.9IyKhGI3Z6UxRLSFInw670wRJciHs0W','1'),(88,'users1','$2y$10$pITJguXXrqZHYRNf1w69jumaUPIwHH8.R/ZliHIH4/EumfVDMazk.','0');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `employees`
---
-ALTER TABLE `employees`
-  ADD PRIMARY KEY (`id_employee`),
-  ADD UNIQUE KEY `user_id_UNIQUE` (`user_id`);
-
---
--- Indexes for table `file_uploaded`
---
-ALTER TABLE `file_uploaded`
-  ADD PRIMARY KEY (`id_info`);
-
---
--- Indexes for table `holiday_request`
---
-ALTER TABLE `holiday_request`
-  ADD PRIMARY KEY (`holiday_id`),
-  ADD KEY `employee_id_idx` (`employee_id`);
-
---
--- Indexes for table `payslip`
---
-ALTER TABLE `payslip`
-  ADD PRIMARY KEY (`payslip_id`),
-  ADD KEY `employee_id_idx` (`employee_id`);
-
---
--- Indexes for table `schedule_rota`
---
-ALTER TABLE `schedule_rota`
-  ADD KEY `shift_id_idx` (`shift_id`),
-  ADD KEY `employee_id_idx` (`employee_id`);
-
---
--- Indexes for table `shift`
---
-ALTER TABLE `shift`
-  ADD PRIMARY KEY (`id_shift`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`users_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `employees`
---
-ALTER TABLE `employees`
-  MODIFY `id_employee` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
---
--- AUTO_INCREMENT for table `file_uploaded`
---
-ALTER TABLE `file_uploaded`
-  MODIFY `id_info` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
---
--- AUTO_INCREMENT for table `holiday_request`
---
-ALTER TABLE `holiday_request`
-  MODIFY `holiday_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=139;
---
--- AUTO_INCREMENT for table `payslip`
---
-ALTER TABLE `payslip`
-  MODIFY `payslip_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
---
--- AUTO_INCREMENT for table `shift`
---
-ALTER TABLE `shift`
-  MODIFY `id_shift` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `employees`
---
-ALTER TABLE `employees`
-  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`users_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `payslip`
---
-ALTER TABLE `payslip`
-  ADD CONSTRAINT `employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id_employee`) ON DELETE CASCADE ON UPDATE CASCADE;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-20 18:34:21
