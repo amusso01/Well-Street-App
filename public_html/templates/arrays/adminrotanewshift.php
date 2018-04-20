2f5dddbcbc4e8bcd09debdb9a5d0d7d11797fb14
andrea <eandram@hotmail.it>
2018-04-20 21:16:09 +0100

    typo adminrotanewshift fix

---
+++ public_html/templates/arrays/adminrotanewshift.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'title'=>'New shift',
+    'FILE_ROOT'=>$FILE_ROOT,
+    'self'=>$self,
+    'user'=>htmlentities($_SESSION['uName']),
+    'homeLink'=>$_SESSION['admin']
+
+);
